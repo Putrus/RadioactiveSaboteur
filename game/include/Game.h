16ddc5ab168a5074fbd2ab0a2bdcfef3ad8df443
Putrus <puertaputra@gmail.com>
2022-05-28 11:06:06 +0200

    Add ResourceHolder class

--- game/include/Game.h
+++ game/include/Game.h
@@ -9,7 +9,7 @@ public:
 
 private:
    void processEvents();
-   void update(sf::Time elapsedTime);
+   void update(sf::Time elapsed_time);
    void render();
 
 private:
---
+++ game/include/ResourceHolder.h
@@ -0,0 +1,48 @@
+#pragma once
+
+template <typename Resource, typename Identifier>
+class ResourceHolder {
+public:
+   void load(Identifier id, const char* filename) {
+      //create and load resource
+      std::unique_ptr<Resource> resource(new Resource());
+      if (!resource->loadFromFile(filename)) {
+         std::string str_filename(filename);
+         throw std::runtime_error("ResourceHolder::load - Failed to load " + str_filename);
+      }
+      //insert resource to map
+      insertResource(id, std::move(resource));
+   }
+
+   template <typename Parameter>
+   void load(Identifier id, const char* filename, const Parameter& second_param) {
+      //create and load resource
+      std::unique_ptr<Resource> resource(new Resource());
+      if (!resource->loadFromFile(filename, second_param)) {
+         std::string str_filename(filename);
+         throw std::runtime_error("ResourceHolder::load - Failed to load " + str_filename);
+      }
+      //insert resource to map
+      insertResource(id, std::move(resource));
+   }
+
+   Resource& get(Identifier id) {
+      return const_cast<Resource&>(const_cast<const ResourceHolder*>(this)->get(id));
+   }
+
+   const Resource& get(Identifier id) const {
+      auto found = m_resource_map.find(id);
+      assert(found != m_resource_map.end());
+
+      return *found->second;
+   }
+
+private:
+   void insertResource(Identifier id, std::unique_ptr<Resource> resource) {
+      auto inserted = m_resource_map.insert(std::make_pair(id, std::move(resource)));
+      assert(inserted.second);
+   }
+
+private:
+   std::map<Identifier, std::unique_ptr<Resource>>	m_resource_map;
+};
