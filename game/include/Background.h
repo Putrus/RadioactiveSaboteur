346792ae06ddc1a546fa929d9d69ced6b9dd46b3
Putrus <puertaputra@gmail.com>
2022-05-28 21:03:36 +0200

    Background update

--- game/include/Background.h
+++ game/include/Background.h
@@ -4,12 +4,19 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+constexpr int background_frame_size = 64;
+constexpr int background_texture_width = 210;
+
 class Background {
 public:
+   Background();
    void draw(sf::RenderWindow& window) const;
-   void addSprite(const sf::Texture& texture, const sf::IntRect& rect);
+   void addSprite(const sf::Texture& texture, const sf::IntRect& rect, const sf::Vector2f& position);
    void update(sf::Time elapsed_time);
 
 private:
    std::vector<std::vector<sf::Sprite>> m_sprites;
+
+   float m_animation_delay;
+   float m_animation_speed;
 };
--- game/src/Background.cpp
+++ game/src/Background.cpp
@@ -1,6 +1,7 @@
 #include "../include/Background.h"
 
-
+Background::Background() : m_animation_delay(0.f), m_animation_speed(0.8f) {
+}
 
 void Background::draw(sf::RenderWindow& window) const {
    for (auto& sprites : m_sprites) {
@@ -10,11 +11,25 @@ void Background::draw(sf::RenderWindow& window) const {
    }
 }
 
-void Background::addSprite(const sf::Texture& texture, const sf::IntRect& rect) {
+void Background::addSprite(const sf::Texture& texture, const sf::IntRect& rect, const sf::Vector2f& position) {
    sf::Sprite sprite(texture);
    sprite.setTextureRect(rect);
+   sprite.setPosition(position);
    if (m_sprites.empty() || m_sprites.back().size() == 64) {
       m_sprites.push_back(std::vector<sf::Sprite>());
    }
    m_sprites.back().push_back(sprite);
+}
+
+void Background::update(sf::Time elapsed_time) {
+   m_animation_delay += elapsed_time.asSeconds();
+   if (m_animation_delay > m_animation_speed) {
+      m_animation_delay -= m_animation_speed;
+      for (auto& sprites : m_sprites) {
+         for (auto& sprite : sprites) {
+            sprite.setTextureRect(sf::IntRect((sprite.getTextureRect().left + background_frame_size) % background_texture_width,
+               sprite.getTextureRect().top, background_frame_size, background_frame_size));
+         }
+      }
+   }
 }
